96bc972fbfe1b0d24c7ca9f3ed6038bd6b40f84b
Veezus Kreist <almighty@veez.us>
2012-12-11 08:44:14 -0600

    Add gitconfig

--- symlink.sh
+++ symlink.sh
@@ -3,3 +3,4 @@ cwd=`pwd`
 [ -e "$HOME/.bashrc.veezus" ] || `ln -sf $cwd/.bashrc.veezus $HOME/.bashrc.veezus`
 [ -e "$HOME/.vimrc" ] || `ln -s $cwd/.vimrc $HOME/.vimrc`
 [ -e "$HOME/.vimrc.veezus" ] || `ln -sf $cwd/.vimrc.veezus $HOME/.vimrc.veezus`
+[ -e "$HOME/.gitconfig" ] || `ln -s $cwd/.gitconfig $HOME/.gitconfig`
