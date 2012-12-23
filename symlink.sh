c5ccc2fd351a7e8b290684e6608bdb7d4bbf0307
Veezus Kreist <almighty@veez.us>
2012-12-23 13:43:01 -0500

    Add .ctags

--- symlink.sh
+++ symlink.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cwd=`pwd`
-files=(.bashrc .bashrc.veezus .vimrc .vimrc.veezus .gitconfig .git-completion.bash)
+files=(.bashrc .bashrc.veezus .ctags .vimrc .vimrc.veezus .gitconfig .git-completion.bash)
 for f in ${files[@]}; do
 	[ -e $HOME/$f ] || `ln -s $cwd/$f $HOME/$f`
 done
