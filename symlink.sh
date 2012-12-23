90076c1aa3b2b356cdb0ef790a327ff44b424a03
Veezus Kreist <almighty@veez.us>
2012-12-23 10:07:21 -0500

    Add tab completion for git in bash

--- symlink.sh
+++ symlink.sh
@@ -4,3 +4,4 @@ cwd=`pwd`
 [ -e "$HOME/.vimrc" ] || `ln -s $cwd/.vimrc $HOME/.vimrc`
 [ -e "$HOME/.vimrc.veezus" ] || `ln -sf $cwd/.vimrc.veezus $HOME/.vimrc.veezus`
 [ -e "$HOME/.gitconfig" ] || `ln -s $cwd/.gitconfig $HOME/.gitconfig`
+[ -e "$HOME/.git-completion.bash" ] || `ln -s $cwd/.git-completion.bash $HOME/.git-completion.bash`
