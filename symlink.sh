ff8d1ca878b963d3eafcfda07d0da8ea699fac55
Veezus Kreist <almighty@veez.us>
2012-12-23 14:09:57 -0500

    Replace symlink.sh with manger bin
    You can now bin/manger link or bin/manger unlink.

--- symlink.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cwd=`pwd`
-files=(.bashrc .bashrc.veezus .ctags .vimrc .vimrc.veezus .gitconfig .git-completion.bash)
-for f in ${files[@]}; do
-	[ -e $HOME/$f ] || `ln -s $cwd/$f $HOME/$f`
-done
