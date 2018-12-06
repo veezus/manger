3716996c8e42610a1bffbe41b6a784fac6eaa253
Veezus Kreist <almighty@veez.us>
2018-12-06 07:24:05 -0600

    Update phoenix config

--- .phoenix.js
+++ .phoenix.js
@@ -4,56 +4,28 @@ let height = 1080;
 let halfLeft = new Key('left', ['alt'], function () {
   let screen = Screen.main().flippedVisibleFrame();
   let window = Window.focused();
-  let spacer = screen.width / 25;
+  let spacer = screen.width / 10;
 
   if (window)
     window.setFrame({
-      x: 5,
-      y: 30,
-      height: screen.height - 50,
-      width: screen.width / 2 - spacer,
+      x: 0,
+      y: 0,
+      height: screen.height,
+      width: screen.width * 0.83,
     });
 });
 
 let halfRight = new Key('right', ['alt'], function () {
   let screen = Screen.main().flippedVisibleFrame();
   let window = Window.focused();
-  let spacer = screen.width / 25;
-
-  if (window)
-    window.setFrame({
-      x: screen.width / 2 + spacer,
-      y: 30,
-      height: screen.height - 50,
-      width: screen.width / 2 - spacer,
-    });
-});
-
-let twoThirdsLeft = new Key('left', ['alt', 'shift'], function () {
-  let screen = Screen.main().flippedVisibleFrame();
-  let window = Window.focused();
   let spacer = screen.width / 10;
 
   if (window)
     window.setFrame({
-      x: 5,
-      y: 30,
-      height: screen.height - 50,
-      width: screen.width * 2 / 3 - spacer,
-    });
-});
-
-let oneThirdRight = new Key('right', ['alt', 'shift'], function () {
-  let screen = Screen.main().flippedVisibleFrame();
-  let window = Window.focused();
-  let spacer = screen.width / 12;
-
-  if (window)
-    window.setFrame({
-      x: screen.width * 2 / 3 - spacer,
-      y: 30,
-      height: screen.height - 50,
-      width: screen.width / 3 + spacer,
+      x: screen.width * 0.20,
+      y: 0,
+      height: screen.height,
+      width: screen.width * 0.80,
     });
 });
 
