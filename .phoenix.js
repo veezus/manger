0be018a1d4311ecbd2573995c9e837cc8c73706c
Veez Remsik <almighty@veez.us>
2022-11-11 09:48:12 -0600

    Update obsidian window size

--- .phoenix.js
+++ .phoenix.js
@@ -69,13 +69,13 @@ let meet = new Key('m', ['alt', 'shift'], function () {
 let notObsidian = new Key('o', ['alt', 'shift'], function () {
   let screen = Screen.main().flippedVisibleFrame();
   let window = Window.focused();
-  let width = screen.width - 1200;
+  let width = screen.width - 1080;
   let height = screen.height; // full height, excludes menubar height
 
   if (window)
     window.setFrame({
       y: 23, // 23px is height of menu bar
-      x: 1200,
+      x: 1080,
       height: height,
       width: width,
     });
@@ -84,7 +84,7 @@ let notObsidian = new Key('o', ['alt', 'shift'], function () {
 let obsidian = new Key('o', ['alt'], function () {
   let screen = Screen.main().flippedVisibleFrame();
   let window = Window.focused();
-  let width = 1200;
+  let width = 1080;
   let height = screen.height; // full height, excludes menubar height
 
   if (window)
